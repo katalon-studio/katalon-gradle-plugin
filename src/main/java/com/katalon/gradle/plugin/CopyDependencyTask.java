1b3b9f407accdb7345d7431b4ac380015f4b05d0
Thong Nguyen <thongmgnguyen@kms-technology.com>
2018-10-09 15:22:22 +0700

    update

---
+++ src/main/java/com/katalon/gradle/plugin/CopyDependencyTask.java
@@ -0,0 +1,18 @@
+package com.katalon.gradle.plugin;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.tasks.TaskAction;
+
+public class CopyDependencyTask extends DefaultTask {
+
+
+  @TaskAction
+  public void copy() {
+    Project project = this.getProject();
+    project.copy(copySpec -> copySpec
+      .from(project.getConfigurations().getByName("compile"))
+      .into("/Drivers")
+      .rename(s -> "katalon_generated_" + s));
+  }
+}
