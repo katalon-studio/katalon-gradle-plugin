2bc5919773643fd40ed41604236f8806aca4303a
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-02-26 15:12:56 +0700

    Automatically add katalon-plugin.json to package

--- src/main/java/com/katalon/gradle/plugin/KatalonGradlePlugin.java
+++ src/main/java/com/katalon/gradle/plugin/KatalonGradlePlugin.java
@@ -6,10 +6,17 @@ import com.katalon.gradle.plugin.list.ListTestSuitesTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.plugins.GroovyPlugin;
 import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.tasks.SourceSet;
+
+import java.util.ArrayList;
 
 public class KatalonGradlePlugin implements Plugin<Project> {
+    private static final String RESOURCE_DIR = "Keywords";
+    private static final String METADATA_FILE = "katalon-plugin.json";
     static final String GROOVY_DEPENDENCY = "org.codehaus.groovy:groovy-all";
     static final String GROOVY_VERSION = "2.4.7";
 
@@ -19,8 +26,25 @@ public class KatalonGradlePlugin implements Plugin<Project> {
         project.getPlugins().apply("com.github.johnrengelman.shadow");
     }
 
+    private void addSourceSets(Project project) {
+        JavaPluginConvention javaPluginConvention = project.getConvention().getPlugin(JavaPluginConvention.class);
+        SourceSet mainSourceSet = javaPluginConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+        SourceDirectorySet resourceDirectorySet = mainSourceSet.getResources();
+
+//        Set directory where resources reside
+        ArrayList<String> resourceDirs = new ArrayList<>();
+        resourceDirs.add(RESOURCE_DIR);
+        resourceDirectorySet.setSrcDirs(resourceDirs);
+
+//        Include only a specific file
+        ArrayList<String> resourceFiles = new ArrayList<>();
+        resourceFiles.add(METADATA_FILE);
+        resourceDirectorySet.setIncludes(resourceFiles);
+    }
+
     public void apply(Project project) {
         applyPlugins(project);
+        addSourceSets(project);
 
         KatalonGradlePluginExtension extension = project.getExtensions()
                 .create("katalon", KatalonGradlePluginExtension.class);
